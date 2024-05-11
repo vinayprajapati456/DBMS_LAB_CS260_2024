74fcf5f8ce8d283ca8c3601a65c6926e2b3a79de
Vinay Prajapati <154899810+vinayprajapati456@users.noreply.github.com>
2024-05-11 22:19:51 +0530

    DBMS Final Project

---
+++ proj1/registration.js
@@ -0,0 +1,17 @@
+let buttton = document.getElementById("save_next");
+buttton.addEventListener("click",function() {
+    // window.location.href = 'registration.html';
+    
+    window.location.href = 'education.html';
+    // window.location.href = 'research.html';
+    
+    // window.location.href = 'research1.html';
+    // window.location.href = 'research2.html';
+    // window.location.href = 'research3.html';
+    // window.location.href = 'research4.html';
+    // window.location.href = 'research5.html';
+    // // window.location.href = 'research6.html';
+    
+
+})
+
